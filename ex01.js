3a9907ea9f89d4935ed08aa47d2a3609161cafa3
Iago Zagnoli Albergaria <iagozag@gmail.com>
2023-03-22 23:14:33 -0300

    funcao que verifica primos adicionada

--- ex01.js
+++ ex01.js
@@ -8,3 +8,10 @@ function two_greater_numbers(list){
     let n = list.length;
     return [list[n-1], list[n]];
 }
+
+function is_prime(x) {
+    for(let i = 2; i < x/2; i++){
+        if(x % i == 0) return false;
+    }
+    return true;
+}
