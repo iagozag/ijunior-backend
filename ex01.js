31638effe0c4e639ca009e10d0baf527d9c24df7
Iago Zagnoli Albergaria <iagozag@gmail.com>
2023-03-22 23:13:20 -0300

    funcao para retornar dois maiores numeros de uma lista  adicionada

--- ex01.js
+++ ex01.js
@@ -2,3 +2,9 @@ function palindrome(str){
   var ans = (str == str.split("").reverse().join("")) ? "YES":"NO";
   return ans;
 }
+
+function two_greater_numbers(list){
+    list.sort();
+    let n = list.length;
+    return [list[n-1], list[n]];
+}
