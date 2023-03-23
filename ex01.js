dbf8f3067580d636a8640b9326ac5c9fd6cc98d2
Iago Zagnoli Albergaria <iagozag@gmail.com>
2023-03-22 23:26:12 -0300

    funcao para contar vogais adicionada

--- ex01.js
+++ ex01.js
@@ -15,3 +15,18 @@ function is_prime(x) {
     }
     return true;
 }
+
+function vowels(str){
+    let vowels = {
+        'a': 0,
+        'e': 0,
+        'i': 0,
+        'o': 0,
+        'u': 0
+    };
+    let str1 = str.toLowerCase();
+    for(let i = 0; i < str1.length; i++){
+        if(str1[i] in vowels) vowels[str1[i]] += 1;
+    }
+    return vowels;
+}
