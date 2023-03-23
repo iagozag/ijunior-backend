6f0f1cf1bf97148d1996174e60c1815ade3be3a8
Iago Zagnoli Albergaria <iagozag@gmail.com>
2023-03-23 19:40:21 -0300

    Update ex01.js

--- ex01.js
+++ ex01.js
@@ -10,9 +10,8 @@ function two_greater_numbers(list){
 }
 
 function is_prime(x) {
-    for(let i = 2; i < x/2; i++){
+    for(let i = 2; i < x/2; i++)
         if(x % i == 0) return false;
-    }
     return true;
 }
 
